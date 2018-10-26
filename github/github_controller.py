68d6a2785f90852bd4e9a5beb096c1cf2c840ded
Michal Polovka <michal.polovka@gmail.com>
2018-10-26 15:48:30 +0200

    WIP print function

--- github/github_controller.py
+++ github/github_controller.py
@@ -1,12 +1,16 @@
+from json import loads
+
+from requests import post
+
+from .api_forms.repositories import list_repositories
 from .authentication import load_api_key
 from .common.InvalidAPIKeyException import InvalidAPIKeyException
 from .logger import logger
-from requests import post
-from .api_forms.repositories import list_repositories
-from json import loads
 
 
 class GithubController:
+    # TODO: Implement print function
+
     __slots__ = 'api_key', 'args'
     api_endpoint = 'https://api.github.com/graphql'
 
@@ -20,8 +24,12 @@ class GithubController:
 
     def obtain_api_key(self):
         """
+        Method reads API key from 'api_key' file in the project root and saves it into class attribute
 
-        :return:
+        :return: None, API key is saved into class attribute api_key
+        :raise: FileNotFoundError in case the 'api_key' file is missing completely
+        :raise: InvalidAPIKeyException in case the 'api_key' file exist, but is either missing the API key
+        or the API key is malformed
         """
         try:
             self.api_key = load_api_key()
@@ -39,22 +47,29 @@ class GithubController:
         """
         actions_dict = {'list-my-repositories': self.list_my_repositories}
         for arg in self.args.action:
+            # TODO: Investigate
             if arg in actions_dict.keys():
                 actions_dict[arg]()
 
     # Repositories operations
 
-    def list_my_repositories(self):
+    def list_my_repositories(self) -> [(str, str, str)]:
         """
+        List all repositories of the current user (authenticated by API key)
 
-        :return:
+        :return: list of tuples (name, sshUrl, url)
         """
         with post(self.api_endpoint, json=list_repositories,
                   headers={"Authorization": "bearer {}".format(self.api_key)}) as response:
             if response.ok:
-                for i in range(loads(response.text)['data']['viewer']['repositories']['totalCount']):
-                    print("{}:".format(i))
-                    print(loads(response.text)['data']['viewer']['repositories']['edges'][i]['node']['name'])
-                    print(loads(response.text)['data']['viewer']['repositories']['edges'][i]['node']['sshUrl'])
-                    print(loads(response.text)['data']['viewer']['repositories']['edges'][i]['node']['url'])
-                    print()
+                total_number_of_repositories = loads(response.text)['data']['viewer']['repositories']['totalCount']
+                logger.debug("Total number of repositories obtainable: {}".format(total_number_of_repositories))
+                repositories_dict = loads(response.text)['data']['viewer']['repositories']['edges']
+                return [
+                    (
+                        repositories_dict[i]['node']['name'],
+                        repositories_dict[i]['node']['sshUrl'],
+                        repositories_dict[i]['node']['url'])
+                    for i in range(total_number_of_repositories)
+                ]
+
